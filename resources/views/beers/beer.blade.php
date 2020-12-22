64ccca42c76844dc9df6e148afb6d56a7e5381cc
777marc@gmail.com <777marc@gmail.com>
2020-12-22 16:47:41 -0500

    misc show details

--- resources/views/beers/beer.blade.php
+++ resources/views/beers/beer.blade.php
@@ -10,11 +10,23 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
 
+                    @php
+                        $ig = $beer->inital_gravity ?? 1.050;
+                        $fg = $beer->final_gravity ?? 1.010;
+                        $abv = ($ig - $fg) * 131;
+                    @endphp
 
                     <div class="beer-card">
-                        {{ $beer->name }}  Brewed on: {{ $beer->brew_date }}
-                        <div class="text-5xl text-yellow-900 p-10">
-                            {{ $beer->final_gravity }}
+                        <div class="text-2xl text-yellow-800 pb-5">
+                            {{ $beer->name }}  Brewed on: {{ Carbon\Carbon::parse($beer->brew_date)->format('m/d/Y') }}
+                        </div>
+                        <div class="text-xl text-yellow-800 pb-5">
+                            ig: {{ $ig }} |
+                            fg: {{ $fg }} |
+                            abv: {{ $abv }}
+                        </div>
+                        <div class="text-lg text-yellow-500">
+                            Bottled on: {{ Carbon\Carbon::parse($beer->bottle_date)->format('m/d/Y') }}
                         </div>
                         <div class="text-lg text-yellow-800">
                             notes: {{ $beer->notes }}
