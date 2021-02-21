7d6cbec2ec259e7a3745186c40c49e35dd6d5ae8
777marc@gmail.com <777marc@gmail.com>
2021-02-21 14:43:12 -0500

    db work

--- config/database.php
+++ config/database.php
@@ -2,6 +2,19 @@
 
 use Illuminate\Support\Str;
 
+if (!env('APP_ENV') == 'local') {
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database = substr($url["path"], 1);
+} else {
+    $host = '';
+    $username = '';
+    $password = '';
+    $database = '';
+}
+
 return [
 
     /*
@@ -62,6 +75,17 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+
+        'heroku_mysql_connection' => array(
+            'driver' => 'mysql',
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+        ),
 
         'pgsql' => [
             'driver' => 'pgsql',
